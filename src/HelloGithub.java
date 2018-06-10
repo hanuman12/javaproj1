552ebf1c54e55c45a55d96e8f23b905cdddac76d
santosh <santosh@192.168.2.22>
2018-06-10 23:53:08 +0530

    remove name

--- src/HelloGithub.java
+++ src/HelloGithub.java
@@ -2,7 +2,7 @@
 public class HelloGithub {
 public static void main(String[] args) {
 	System.out.println("Hello GitHub");
-	System.out.println("Hello Anya");
+	System.out.println("Hell ");
 	System.out.println("............AAA..........");
 	System.out.println("Hello Tantya");
 	
